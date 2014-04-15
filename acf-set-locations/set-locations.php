17f174013f80e3259e8704fa0b984d18b59d8dd7
Andrew Baxter <andrew@toughbaxter.com>
2014-04-14 20:36:25 -0700

    Clearing unused ACF functions

--- acf-set-locations/set-locations.php
+++ acf-set-locations/set-locations.php
@@ -224,25 +224,6 @@ class acf_field_set_locations extends acf_field
 	
 	
 	/*
-	*  create_options()
-	*
-	*  Create extra options for your field. This is rendered when editing a field.
-	*  The value of $field['name'] can be used (like bellow) to save extra data to the $field
-	*
-	*  @type	action
-	*  @since	3.6
-	*  @date	23/01/13
-	*
-	*  @param	$field	- an array holding all the field's data
-	*/
-	
-	function create_options( $field )
-	{
-		
-	}
-	
-	
-	/*
 	*  format_value_for_api()
 	*
 	*  This filter is appied to the $value after it is loaded from the db and before it is passed back to the api functions such as the_field
@@ -261,216 +242,12 @@ class acf_field_set_locations extends acf_field
 	function format_value_for_api( $value, $post_id, $field )
 	{
 		
-		// validate
-		if( !$value )
-		{
-			return false;
-		}
-		
-		
-		// format
-		if( $field['save_format'] == 'url' )
-		{
-			$value = wp_get_attachment_url( $value );
-		}
-		elseif( $field['save_format'] == 'object' )
-		{
-			$attachment = get_post( $value );
-			
-			
-			// validate
-			if( !$attachment )
-			{
-				return false;	
-			}
-			
-			
-			// create array to hold value data
-			$src = wp_get_attachment_image_src( $attachment->ID, 'full' );
-			
-			$value = array(
-				'id' => $attachment->ID,
-				'alt' => get_post_meta($attachment->ID, '_wp_attachment_image_alt', true),
-				'title' => $attachment->post_title,
-				'caption' => $attachment->post_excerpt,
-				'description' => $attachment->post_content,
-				'mime_type'	=> $attachment->post_mime_type,
-				'url' => $src[0],
-				'width' => $src[1],
-				'height' => $src[2],
-				'sizes' => array(),
-			);
-			
-			
-			// find all image sizes
-			$image_sizes = get_intermediate_image_sizes();
-			
-			if( $image_sizes )
-			{
-				foreach( $image_sizes as $image_size )
-				{
-					// find src
-					$src = wp_get_attachment_image_src( $attachment->ID, $image_size );
-					
-					// add src
-					$value[ 'sizes' ][ $image_size ] = $src[0];
-					$value[ 'sizes' ][ $image_size . '-width' ] = $src[1];
-					$value[ 'sizes' ][ $image_size . '-height' ] = $src[2];
-				}
-				// foreach( $image_sizes as $image_size )
-			}
-			// if( $image_sizes )
-			
-		}
-		
-		return $value;
 		
 	}
 	
-	
-	/*
-	*  get_media_item_args
-	*
-	*  @description: 
-	*  @since: 3.6
-	*  @created: 27/01/13
-	*/
-	
-	function get_media_item_args( $vars )
-	{
-	    $vars['send'] = true;
-	    return($vars);
-	}
-	
-	
-	/*
-   	*  ajax_get_images
-   	*
-   	*  @description: 
-   	*  @since: 3.5.7
-   	*  @created: 13/01/13
-   	*/
-	
-   	function ajax_get_images()
-   	{
-   		// vars
-		$options = array(
-			'nonce' => '',
-			'images' => array(),
-			'preview_size' => 'thumbnail'
-		);
-		$return = array();
-		
-		
-		// load post options
-		$options = array_merge($options, $_POST);
-		
-		
-		// verify nonce
-		if( ! wp_verify_nonce($options['nonce'], 'acf_nonce') )
-		{
-			die(0);
-		}
-		
-		
-		if( $options['images'] )
-		{
-			foreach( $options['images'] as $id )
-			{
-				$url = wp_get_attachment_image_src( $id, $options['preview_size'] );
-				
-				
-				$return[] = array(
-					'id' => $id,
-					'url' => $url[0],
-				);
-			}
-		}
-		
-		
-		// return json
-		echo json_encode( $return );
-		die;
-		
-   	}
-   		
-	
-	/*
-	*  image_size_names_choose
-	*
-	*  @description: 
-	*  @since: 3.5.7
-	*  @created: 13/01/13
-	*/
-	
-	function image_size_names_choose( $sizes )
-	{
-		global $_wp_additional_image_sizes;
-			
-		if( $_wp_additional_image_sizes )
-		{
-			foreach( $_wp_additional_image_sizes as $k => $v )
-			{
-				$title = $k;
-				$title = str_replace('-', ' ', $title);
-				$title = str_replace('_', ' ', $title);
-				$title = ucwords( $title );
-				
-				$sizes[ $k ] = $title;
-			}
-			// foreach( $image_sizes as $image_size )
-		}
-		
-        return $sizes;
-	}
-	
-	
-	/*
-	*  wp_prepare_attachment_for_js
-	*
-	*  @description: This sneaky hook adds the missing sizes to each attachment in the 3.5 uploader. It would be a lot easier to add all the sizes to the 'image_size_names_choose' filter but then it will show up on the normal the_content editor
-	*  @since: 3.5.7
-	*  @created: 13/01/13
-	*/
-	
-	function wp_prepare_attachment_for_js( $response, $attachment, $meta )
-	{
-		// only for image
-		if( $response['type'] != 'image' )
-		{
-			return $response;
-		}
-		
-		
-		// make sure sizes exist. Perhaps they dont?
-		if( !isset($meta['sizes']) )
-		{
-			return $response;
-		}
-		
-		
-		$attachment_url = $response['url'];
-		$base_url = str_replace( wp_basename( $attachment_url ), '', $attachment_url );
-		
-		if( isset($meta['sizes']) && is_array($meta['sizes']) )
-		{
-			foreach( $meta['sizes'] as $k => $v )
-			{
-				if( !isset($response['sizes'][ $k ]) )
-				{
-					$response['sizes'][ $k ] = array(
-						'height'      =>  $v['height'],
-						'width'       =>  $v['width'],
-						'url'         => $base_url .  $v['file'],
-						'orientation' => $v['height'] > $v['width'] ? 'portrait' : 'landscape',
-					);
-				}
-			}
-		}
 
-		return $response;
-	}
-	
+   		
+
 	
 	/*
 	*  update_value()
@@ -490,19 +267,7 @@ class acf_field_set_locations extends acf_field
 	
 	function update_value( $value, $post_id, $field )
 	{
-		// array?
-		if( is_array($value) && isset($value['id']) )
-		{
-			$value = $value['id'];	
-		}
-		
-		// object?
-		if( is_object($value) && isset($value->ID) )
-		{
-			$value = $value->ID;
-		}
-		
-		return $value;
+
 	}
 	
 	
